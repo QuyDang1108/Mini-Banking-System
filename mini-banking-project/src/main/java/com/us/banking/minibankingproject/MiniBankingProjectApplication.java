f0cacc3479e2da903b8c8a264f0a6e5ddd150a16
DangQuy <dangquyy1108@gmail.com>
2025-10-22 13:43:28 +0700

    init project

---
+++ mini-banking-project/src/main/java/com/us/banking/minibankingproject/MiniBankingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.us.banking.minibankingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBankingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniBankingProjectApplication.class, args);
+    }
+
+}
---
+++ mini-banking-project/src/test/java/com/us/banking/minibankingproject/MiniBankingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.us.banking.minibankingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBankingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
